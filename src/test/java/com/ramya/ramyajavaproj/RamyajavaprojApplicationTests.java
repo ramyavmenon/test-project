257af59d01cdede74e214637c5393445c41e82f6
ramyavmenon <ramyavmenon@gmail.com>
2021-02-15 15:41:19 -0500

    Add files via upload

---
+++ src/test/java/com/ramya/ramyajavaproj/RamyajavaprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramya.ramyajavaproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamyajavaprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
